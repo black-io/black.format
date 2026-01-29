ed71ad14bb59492e5bd43f9e0ba79ffafed3397b
Frank Stein <dr.frank.stain@gmail.com>
2026-01-29 11:51:42 +0300

    Add the most common blocks of ZIP format.

--- include/zip/internal/constants.h
+++ include/zip/internal/constants.h
@@ -16,14 +16,14 @@ namespace Internal
 	*/
 	enum class HeaderSignature : uint32_t
 	{
-		LocalFileHeader						= '\x50\x4B\x03\x04',	// (4.3.7) Local file header signature.
-		DataDescriptor						= '\x50\x4B\x07\x08',	// (4.3.9) File data descriptor signature.
-		ArchiveExtraDataRecord				= '\x50\x4B\x06\x08',	// (4.3.11) Archive extra-data record signature.
-		CentralDirectoryFileHeader			= '\x50\x4B\x01\x02',	// (4.3.12) File header in central directory.
-		CentralDirectoryDigitalSignature	= '\x50\x4B\x05\x05',	// (4.3.13) Digital signature of central directory.
-		Zip64EndOfCentralDirectory			= '\x50\x4B\x06\x06',	// (4.3.14) EOCD signature for Zip64.
-		Zip64EndOfCentralDirectoryLocator	= '\x50\x4B\x06\x07',	// (4.3.15) EOCD locator signature for Zip64.
-		EndOfCentralDirectory				= '\x50\x4B\x05\x06',	// (4.3.16) EOCD signature.
+		LocalFileHeader						= '\x04\x03\x4B\x50',	// (4.3.7) Local file header signature.
+		DataDescriptor						= '\x08\x07\x4B\x50',	// (4.3.9) File data descriptor signature.
+		ArchiveExtraDataRecord				= '\x08\x06\x4B\x50',	// (4.3.11) Archive extra-data record signature.
+		CentralDirectoryFileHeader			= '\x02\x01\x4B\x50',	// (4.3.12) File header in central directory.
+		CentralDirectoryDigitalSignature	= '\x05\x05\x4B\x50',	// (4.3.13) Digital signature of central directory.
+		Zip64EndOfCentralDirectory			= '\x06\x06\x4B\x50',	// (4.3.14) EOCD signature for Zip64.
+		Zip64EndOfCentralDirectoryLocator	= '\x07\x06\x4B\x50',	// (4.3.15) EOCD locator signature for Zip64.
+		EndOfCentralDirectory				= '\x06\x05\x4B\x50',	// (4.3.16) EOCD signature.
 	};
 }
 }
--- include/zip/internal/structures.h
+++ include/zip/internal/structures.h
@@ -10,25 +10,49 @@ inline namespace Zip
 namespace Internal
 {
 	/**
+		@brief	Local File Header description.
+
+		This header described in section 4.3.6 of .ZIP file format specification.
+
+		4.3.7  Local file header:
+			local file header signature     4 bytes  (0x04034b50)
+			version needed to extract       2 bytes
+			general purpose bit flag        2 bytes
+			compression method              2 bytes
+			last mod file time              2 bytes
+			last mod file date              2 bytes
+			crc-32                          4 bytes
+			compressed size                 4 bytes
+			uncompressed size               4 bytes
+			file name length                2 bytes
+			extra field length              2 bytes
+
+			file name (variable size)
+			extra field (variable size)
+
+		30 bytes total.
+		This is only header. It does not include the strings of file name and extra field.
+
+		The `SIGNATURE` static field stores the valid value of local file header signature. So it can be used during the parsing of format.
 	*/
 	#pragma pack( push, 2 )
 	struct LocalFileHeader final
 	{
-		//
+		// Should be 0x04034b50. Valid signature of header.
 		static constexpr HeaderSignature SIGNATURE = HeaderSignature::LocalFileHeader;
 
 
-		HeaderSignature	signature				= SIGNATURE;
-		uint16_t		extractor_version		= 0;
-		uint16_t		general_purpose_bits	= 0;
-		uint16_t		compression_function	= 0;
-		uint16_t		last_modification_time	= 0;
-		uint16_t		last_modification_date	= 0;
-		uint32_t		checksumm				= 0;
-		uint32_t		compressed_length		= 0;
-		uint32_t		uncompressed_length		= 0;
-		uint16_t		name_length				= 0;
-		uint16_t		extra_field_length		= 0;
+		HeaderSignature	signature				= SIGNATURE;	// [4B] {0x04034b50} local file header signature
+		uint16_t		extractor_version		= 0;			// [2B] (4.4.3) version needed to extract
+		uint16_t		general_purpose_bits	= 0;			// [2B] (4.4.4) general purpose bit flag
+		uint16_t		compression_function	= 0;			// [2B] (4.4.5) compression method
+		uint16_t		last_modification_time	= 0;			// [2B] (4.4.6) last mod file time
+		uint16_t		last_modification_date	= 0;			// [2B] (4.4.6) last mod file date
+		uint32_t		checksumm				= 0;			// [4B] (4.4.7) crc-32
+		uint32_t		compressed_length		= 0;			// [4B] (4.4.8) compressed size
+		uint32_t		uncompressed_length		= 0;			// [4B] (4.4.9) uncompressed size
+		uint16_t		name_length				= 0;			// [2B] (4.4.10) file name length
+		uint16_t		extra_field_length		= 0;			// [2B] (4.4.11) extra field length
 	};
 	#pragma pack( pop )
 
