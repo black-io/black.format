3e565125d511961db0135de49bae648d51daefe5
Frank Stein <dr.frank.stain@gmail.com>
2026-02-18 18:21:24 +0300

    First information for ZIP format aliases.

--- include/zip/internal/aliases.h
+++ include/zip/internal/aliases.h
@@ -10,6 +10,12 @@ inline namespace Zip
 namespace Internal
 {
 	/**
+		@brief	General purpose bit flags.
+
+		Functionality of this type described in section 4.4.4 of .ZIP file format specification.
+
+		This type implements the useful interface to operate with bit flags in ZIP headers.
+		Instead of bit flag description, implemented in `GeneralPurposeBitFlag`, this type implements only handy usage interface.
 	*/
 	using GeneralPurposeBitFlags = Black::EnumFlags<GeneralPurposeBitFlag>;
 }
