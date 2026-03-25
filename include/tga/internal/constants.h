ab300699594d4ea7b376cf3fc42b014aa47438ac
Frank Stein <dr.frank.stain@gmail.com>
2026-03-25 11:56:17 +0300

    TGA content bit-rate information.

--- include/tga/internal/constants.h
+++ include/tga/internal/constants.h
@@ -34,6 +34,37 @@ namespace Internal
 		RleRgba				= 10,	// Run-length encoded, True-color Image.
 		RleGrayscale		= 11,	// Run-length encoded, Black-and-white Image.
 	};
+
+	/**
+		@brief	TGA image bit-rate specification.
+
+		This enumeration explained in next sections of TGA 2.0 file format specification:
+		- `Field 4.3 (1 byte) - Color map Entry Size`;
+		- `Field 5.5 (1 byte) - Pixel Depth`.
+		The description in this sections differ from each other, but the overall conclusion makes it real to combine both fields into same enumeration.
+
+		The user should note that bit-rate of image describes total size of single element in bits.
+		In case of paletted image, bit-rate of image describes the size of color index. And even in this case the image can store images with alpha-channel.
+
+		Description of values uses some characters in next notation:
+		- `<Char><Value>` is channel specification, where the `Value` means number of bits used and `Char` is one of next;
+		- `R` - Red channel;
+		- `G` - Green channel;
+		- `B` - Blue channel;
+		- `A` - Alpha channel;
+		- `W` - Black to White magnitude;
+		- `I` - index in color palette.
+		- `X` - Irrelevant data;
+		- All channel specifications are written in "hi->lo" order, representing the Little-endian byte order.
+	*/
+	enum class Bitrate : uint8_t
+	{
+		Undefined	= 0,	// Undefined bit-rate.
+		Monochrome	= 8,	// 8Bpp W8 color or I8 color index in palette.
+		ARGB16		= 16,	// 15+1Bpp X1R5G5B5 color or 16Bpp A1R5G5B5 color. Also may represent I16 or A8I8 color index in palette.
+		RGB24		= 24,	// 24Bpp R8G8B8 color. Also may be represent A8I16 color index in palette.
+		ARGB32		= 32,	// 24+8Bpp X8R8G8B8 color or 32Bpp A8R8G8B8 color.
+	};
 }
 }
 }
