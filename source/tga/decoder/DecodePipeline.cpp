d7322f747cf44419bcbef9061a34ca1ee78aef53
Frank Stein <dr.frank.stain@gmail.com>
2026-03-31 11:57:06 +0300

    Configuration of image input feeder.

---
+++ source/tga/decoder/DecodePipeline.cpp
@@ -0,0 +1,47 @@
+#include "decoder.h"
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Tga
+{
+namespace Decoder
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/TGA/DecodePipeline";
+}
+
+
+	const Black::BooleanStatus DecodePipeline::SetupInputFeeder( const Internal::Header& header, const Black::PlainView<const std::byte>& image_buffer )
+	{
+		switch( Internal::ClassifyContentCompression( header.content_type ) )
+		{
+		case Internal::ContentCompression::None:
+			{
+				Decoder::StraightInputFeeder& feeder = ConstructComponent<Decoder::StraightInputFeeder>();
+				feeder.UseImageBuffer( image_buffer );
+				feeder.UseBitrate( header.image.bitrate );
+			}
+			return Black::BooleanStatus::Success;
+		case Internal::ContentCompression::Rle:
+			{
+				Decoder::RleInputFeeder& feeder = ConstructComponent<Decoder::RleInputFeeder>();
+				feeder.UseImageBuffer( image_buffer );
+				feeder.UseBitrate( header.image.bitrate );
+			}
+			return Black::BooleanStatus::Success;
+		default:
+			break;
+		}
+
+		BLACK_LOG_ERROR( LOG_CHANNEL, "Failed to determine compression of image input buffer." );
+		return BooleanStatus::Failure;
+	}
+}
+}
+}
+}
