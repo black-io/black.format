31e1e11067b8863129606b63852ddecb9ae715ee
Frank Stein <dr.frank.stain@gmail.com>
2026-03-31 11:12:48 +0300

    First code for image decoder input feeder.

---
+++ source/tga/decoder/BasicInputFeeder.h
@@ -0,0 +1,77 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Tga
+{
+namespace Decoder
+{
+	/**
+	*/
+	class BasicInputFeeder : private Black::NonTransferable
+	{
+	// Public inner types.
+	public:
+		//
+		using Bitrate = Black::TgaStructure::Bitrate;
+
+	// Public interface.
+	public:
+		//
+		void UseImageBuffer( Black::PlainView<const std::byte> image_buffer );
+
+		//
+		void UseBitrate( const Bitrate bitrate );
+
+
+		//
+		void Rewind();
+
+		//
+		const Black::BooleanStatus SkipElement();
+
+
+		//
+		inline const std::byte* PeekElement() const	{ return m_current_element; };
+
+		//
+		inline const size_t GetElementSize() const	{ return m_element_size; };
+
+		//
+		inline const Bitrate GetBitrate() const		{ return m_bitrage; };
+
+
+		//
+		inline const bool HasElements() const		{ return m_current_element != m_image_buffer.GetEnd(); };
+
+	// Heirs life-time management.
+	protected:
+		inline BasicInputFeeder() noexcept	= default;
+		inline ~BasicInputFeeder() noexcept	= default;
+
+	// Heirs interface.
+	protected:
+
+	// Heirs virtual interface.
+	protected:
+		//
+		virtual void OnRewind() = 0;
+
+		//
+		virtual const Black::BooleanStatus OnSkipElement() = 0;
+
+	// Heirs common state.
+	private:
+		Black::PlainView<const std::byte>	m_image_buffer;
+		const std::byte*					m_current_element	= nullptr;
+		size_t								m_element_size		= 0;
+
+		Bitrate m_bitrage = Bitrate::Undefined;
+	};
+}
+}
+}
+}
