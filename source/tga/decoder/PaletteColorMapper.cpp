0fb35b575515f8803a54d8f998d90409c71c449e
Frank Stein <dr.frank.stain@gmail.com>
2026-04-02 13:58:31 +0300

    First code of palette color mapper.

---
+++ source/tga/decoder/PaletteColorMapper.cpp
@@ -0,0 +1,59 @@
+#include "decoder.h"
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Tga
+{
+namespace Decoder
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/TGA/Decoder/ColorMapper";
+}
+
+
+	void PaletteColorMapper::UsePalette( Black::PlainView<const std::byte> palette_buffer, const Internal::Bitrate bitrate )
+	{
+		m_palette				= std::move( palette_buffer );
+		m_bitrate				= bitrate;
+		m_palete_element_size	= Internal::GetElementSize( m_bitrate );
+	}
+
+	const uint32_t PaletteColorMapper::PeekPaletteElement( const size_t element_index ) const
+	{
+		uint32_t result = 0;
+
+		const size_t element_offset = element_index * m_palete_element_size;
+		EXPECTS_DEBUG( element_offset < m_palette.GetLength() );
+
+		Black::CopyMemory( &result, &m_palette[ element_offset ], m_palete_element_size );
+
+		return result;
+	}
+
+	const uint32_t PaletteColorMapper::PerformPeekElement() const
+	{
+		uint32_t result = 0;
+
+		const Black::ImageFormat& input_format = GetInputFormat();
+		Black::CopyMemory( &result, GetInputFeeder().PeekElement(), input_format.size_bytes );
+
+		const size_t index		= ( input_format.has_alpha )? ( result & GetInputColorMask() ) : result;
+		const uint32_t alpha	= ( ( input_format.has_alpha )? ( result >> GetInputFirstAlphaBit() ) : ~uint32_t{} ) & 0xFFUL;
+
+		result = PeekPaletteElement( index );
+		if( input_format.has_alpha )
+		{
+			result = result | ( ( alpha << m_output_first_alpha_bit ) & m_output_alpha_mask );
+		}
+
+		return result;
+	}
+}
+}
+}
+}
---
+++ source/tga/decoder/PaletteColorMapper.h
@@ -0,0 +1,51 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Tga
+{
+namespace Decoder
+{
+	/**
+	*/
+	class PaletteColorMapper final : private Black::HostRelatedComponent<DecodePipeline>, public BasicColorMapper
+	{
+	// Public life-time management.
+	public:
+		PaletteColorMapper() noexcept	= default;
+		~PaletteColorMapper() noexcept	= default;
+
+	// Public interface.
+	public:
+		//
+		void UsePalette( Black::PlainView<const std::byte> palette_buffer, const Internal::Bitrate bitrate );
+
+	// Private interface.
+	private:
+		//
+		const uint32_t PeekPaletteElement( const size_t element_index ) const;
+
+	// `BasicColorMapper` interface implementations.
+	private:
+		//
+		const uint32_t PerformPeekElement() const final;
+
+	// Private state.
+	private:
+		Black::PlainView<const std::byte>	m_palette;
+
+		size_t				m_output_first_alpha_bit	= 0;
+		size_t				m_output_color_mask			= 0;
+		size_t				m_output_alpha_mask			= 0;
+		size_t				m_palete_element_size		= 0;
+		Black::ImageFormat	m_palete_format				= Black::ImageFormats::UNDEFINED;
+
+		Internal::Bitrate	m_bitrate					= Internal::Bitrate::Undefined;
+	};
+}
+}
+}
+}
