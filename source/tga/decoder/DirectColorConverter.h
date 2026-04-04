89060e7be222dfb221beaf217ef2fd6b470765c0
Frank Stein <dr.frank.stain@gmail.com>
2026-04-04 13:04:34 +0300

    Stub files for direct output color converter.

---
+++ source/tga/decoder/DirectColorConverter.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Tga
+{
+namespace Decoder
+{
+	/**
+	*/
+	class DirectColorConverter final : private Black::HostRelatedComponent<DecodePipeline>, public BasicColorConverter
+	{
+	// Public life-time management.
+	public:
+		using Black::HostRelatedComponent<DecodePipeline>::HostRelatedComponent;
+		~DirectColorConverter() noexcept = default;
+
+	// `BasicColorMapper` interface implementations.
+	private:
+		//
+		const Black::BooleanStatus PerformColorConversion( const uint32_t color, const Black::ImageFormat color_format ) const final;
+	};
+}
+}
+}
+}
