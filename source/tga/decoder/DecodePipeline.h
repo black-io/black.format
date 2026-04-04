1f45fbfe7e3c68c0b15db6d673d82d8438c8d81e
Frank Stein <dr.frank.stain@gmail.com>
2026-04-04 20:35:33 +0300

    Add new converter into pipeline.

--- source/tga/decoder/DecodePipeline.h
+++ source/tga/decoder/DecodePipeline.h
@@ -16,7 +16,7 @@ namespace Decoder
 			DecodePipeline,
 			Black::TypesUnion<StraightInputFeeder, RleInputFeeder>,
 			Black::TypesUnion<PaletteColorMapper, TrueColorMapper, MonochromeColorMapper>,
-			Black::TypesUnion<DirectColorConverter, RemappingColorConverter, TransformColorConverter>
+			Black::TypesUnion<MonochromeColorConverter, DirectColorConverter, RemappingColorConverter, TransformColorConverter>
 		>
 	{
 	// Public interface.
