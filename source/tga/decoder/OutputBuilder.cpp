65c4e40614e8f8aaa73cce78e759bd9debaba115
Frank Stein <dr.frank.stain@gmail.com>
2026-04-05 23:23:24 +0300

    First view of output builder code.

--- source/tga/decoder/OutputBuilder.cpp
+++ source/tga/decoder/OutputBuilder.cpp
@@ -16,9 +16,31 @@ namespace
 }
 
 
-	const Black::BooleanStatus BasicOutputBuilder::ProduceElement( const uint32_t color )
+	void OutputBuilder::UseCursor( const CoordinateCursor& cursor )
 	{
-		return PerformElementProduction( color );
+		m_cursor = &cursor;
+	}
+
+	void OutputBuilder::UseOutputBuffer( Black::PlainView<std::byte> output_buffer )
+	{
+		m_output_buffer = std::move( output_buffer );
+	}
+
+	void OutputBuilder::UseOutputFormat( const Black::ImageFormat output_format )
+	{
+		m_output_format = output_format;
+	}
+
+	const Black::BooleanStatus OutputBuilder::ProduceElement( const uint32_t color )
+	{
+		CRET( m_cursor == nullptr, Black::BooleanStatus::Failure );
+		EXPECTS_DEBUG( m_output_format.size_bits > 0 );
+
+		const size_t offset = m_output_format.size_bytes * m_cursor->GetOutputIndex();
+		CRET( offset >= m_output_buffer.GetLength(), Black::BooleanStatus::Failure );
+
+		Black::CopyMemory( &m_output_buffer[ offset ], &color, m_output_format.size_bytes );
+		return Black::BooleanStatus::Success;
 	}
 }
 }
