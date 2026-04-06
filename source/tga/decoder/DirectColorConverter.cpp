bd15b62296000f54c8d7514b1e65082154da5fe5
Frank Stein <dr.frank.stain@gmail.com>
2026-04-06 13:54:12 +0300

    Fix direct color converter to use color format operator.

--- source/tga/decoder/DirectColorConverter.cpp
+++ source/tga/decoder/DirectColorConverter.cpp
@@ -18,14 +18,15 @@ namespace
 
 	const Black::BooleanStatus DirectColorConverter::PerformColorConversion( const uint32_t color, const Black::ColorFormat color_format ) const
 	{
-		uint32_t converted_color = color;
+		uint64_t converted_color = color;
 
-		if( !color_format.has_alpha && GetOutputFormat().has_alpha )
+		const Black::ColorFormatOperator& output_operator = GetOutputOperator();
+		if( !color_format.has_alpha && output_operator.CanProcessAlphaChannel() )
 		{
-			converted_color = converted_color | uint32_t( GetOutputOperator().GetAlphaChannelMask() );
+			converted_color = output_operator.ReplaceAlphaChannel( converted_color, output_operator.GetAlphaChannelMask() );
 		}
 
-		return GetOutputBuilder().ProduceElement( converted_color );
+		return GetOutputBuilder().ProduceElement( uint32_t( converted_color ) );
 	}
 }
 }
