c5cf8fa66c5ec8e67f35e0ef0a3684c17f632798
Frank Stein <dr.frank.stain@gmail.com>
2026-04-09 11:16:23 +0300

    Fix the source code of output builder according to new class declaration.

--- source/tga/decoder/OutputBuilder.cpp
+++ source/tga/decoder/OutputBuilder.cpp
@@ -17,8 +17,7 @@ namespace
 
 
 	OutputBuilder::OutputBuilder( OutputBuilder&& other ) noexcept
-		: m_cursor{ std::exchange( other.m_cursor, nullptr ) }
-		, m_output_buffer{ std::move( other.m_output_buffer ) }
+		: m_output_buffer{ std::move( other.m_output_buffer ) }
 		, m_output_format{ std::exchange( other.m_output_format, Black::ColorFormats::UNDEFINED ) }
 	{
 	}
@@ -32,41 +31,18 @@ namespace
 
 	void OutputBuilder::Swap( OutputBuilder& other )
 	{
-		Black::Swap( m_cursor, other.m_cursor );
 		Black::Swap( m_output_buffer, other.m_output_buffer );
 		Black::Swap( m_output_format, other.m_output_format );
 	}
 
-	void OutputBuilder::RefuseCursor()
+	void OutputBuilder::ProduceElement( const uint32_t color, const CoordinateCursor& cursor ) const
 	{
-		m_cursor = nullptr;
-	}
-
-	void OutputBuilder::UseCursor( const CoordinateCursor& cursor )
-	{
-		m_cursor = &cursor;
-	}
-
-	void OutputBuilder::UseOutputBuffer( Black::PlainView<std::byte> output_buffer )
-	{
-		m_output_buffer = std::move( output_buffer );
-	}
-
-	void OutputBuilder::UseOutputFormat( const Black::ColorFormat output_format )
-	{
-		m_output_format = output_format;
-	}
-
-	const Black::BooleanStatus OutputBuilder::ProduceElement( const uint32_t color )
-	{
-		CRET( m_cursor == nullptr, Black::BooleanStatus::Failure );
 		EXPECTS_DEBUG( m_output_format.size_bits > 0 );
 
-		const size_t offset = m_output_format.size_bytes * m_cursor->GetOutputIndex();
-		CRET( offset >= m_output_buffer.GetLength(), Black::BooleanStatus::Failure );
+		const size_t offset = m_output_format.size_bytes * cursor.GetOutputIndex();
+		ENSURES_DEBUG( offset < m_output_buffer.GetLength() );
 
 		Black::CopyMemory( &m_output_buffer[ offset ], &color, m_output_format.size_bytes );
-		return Black::BooleanStatus::Success;
 	}
 }
 }
