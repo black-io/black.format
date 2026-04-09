c4659f7bddabb44fa11e0b5ac8787fb40a95ab67
Frank Stein <dr.frank.stain@gmail.com>
2026-04-09 10:40:38 +0300

    Add docs for output builder of TGA image decoder.

--- source/tga/decoder/OutputBuilder.h
+++ source/tga/decoder/OutputBuilder.h
@@ -10,6 +10,13 @@ inline namespace Tga
 namespace Decoder
 {
 	/**
+		@brief	Output image builder for image decoding pipeline.
+
+		Output builder is the final, the fourth, step in TGA image decoding pipeline. It builds the output image
+		using position of cursor and color converted from input.
+
+		Main thing of output builder is the memory to store the output image.
+		Coordinate cursor is used to place the input color at right output position.
 	*/
 	class OutputBuilder final
 	{
@@ -30,31 +37,31 @@ namespace Decoder
 
 	// Public interface.
 	public:
-		//
+		// Swap the state of this builder with other.
 		void Swap( OutputBuilder& other );
 
-		//
+		// Instruct the builder to stop the using of previously given coordinate cursor.
 		void RefuseCursor();
 
-		//
+		// Instruct the builder to use given coordinate cursor.
 		void UseCursor( const CoordinateCursor& cursor );
 
-		//
+		// Instruct the builder to use given buffer as output image memory.
 		void UseOutputBuffer( Black::PlainView<std::byte> output_buffer );
 
-		//
+		// Instruct he builder to use given format of output image.
 		void UseOutputFormat( const Black::ColorFormat output_format );
 
 
-		//
+		// Produce the single pixel of output image, using position of coordinate cursor along with given color.
 		const Black::BooleanStatus ProduceElement( const uint32_t color );
 
 	// Private state.
 	private:
-		const CoordinateCursor*		m_cursor			= nullptr;
-		Black::PlainView<std::byte>	m_output_buffer;
+		const CoordinateCursor*		m_cursor			= nullptr;							// Configured coordinate cursor.
+		Black::PlainView<std::byte>	m_output_buffer;										// Memory of output image.
 
-		Black::ColorFormat			m_output_format		= Black::ColorFormats::UNDEFINED;
+		Black::ColorFormat			m_output_format		= Black::ColorFormats::UNDEFINED;	// Output image format.
 	};
 }
 }
