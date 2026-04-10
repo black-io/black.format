7c7d6a798642bb0067cf11fd09a7475df8f96df6
Frank Stein <dr.frank.stain@gmail.com>
2026-04-10 12:47:24 +0300

    First look of master-header for PNG format subsystem.

---
+++ include/png/png.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+// Fundamental external dependencies.
+
+
+namespace Black
+{
+inline namespace Format
+{
+// PNG file format subsystem.
+inline namespace Png
+{
+// Internal definitions for PNG format.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Internal stuff.
+#include "internal/constants.h"
+#include "internal/structures.h"
+
+// PNG file format types.
+#include "PngStructure.h"
+#include "PngFileView.h"
