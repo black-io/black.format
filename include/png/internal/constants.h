18ef9f1ffd7d55ea3b1c7a816e3c7d9496f8b271
Frank Stein <dr.frank.stain@gmail.com>
2026-04-10 13:00:52 +0300

    Add the preambula of PNG file.

---
+++ include/png/internal/constants.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Png
+{
+namespace Internal
+{
+	//
+	inline constexpr uint8_t PNG_PREAMBULA[] { 137, 80, 78, 71, 13, 10, 26, 10 };
+}
+}
+}
+}
