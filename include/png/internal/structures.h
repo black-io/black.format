e45328367de2f32a09cd2d616f03920cdec71d23
Frank Stein <dr.frank.stain@gmail.com>
2026-04-11 16:00:39 +0300

    Remove the header of chunk.

--- include/png/internal/structures.h
+++ include/png/internal/structures.h
@@ -10,16 +10,10 @@ inline namespace Png
 namespace Internal
 {
 	//
-	struct ChunkHeader final
-	{
-		size32_t	content_size;
-		uint32_t	type_code;
-	};
-
-	//
 	struct Chunk final
 	{
-		ChunkHeader					header;
+		size32_t					content_size;
+		uint32_t					type_code;
 		Black::PlainView<std::byte>	content;
 		uint32_t					checksumm;
 	};
