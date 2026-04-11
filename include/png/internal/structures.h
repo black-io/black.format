784ccae58579db5aefb1a701c6d41a519ffa9783
Frank Stein <dr.frank.stain@gmail.com>
2026-04-11 16:01:00 +0300

    Add PNG image header.

--- include/png/internal/structures.h
+++ include/png/internal/structures.h
@@ -17,6 +17,21 @@ namespace Internal
 		Black::PlainView<std::byte>	content;
 		uint32_t					checksumm;
 	};
+
+	/**
+	*/
+	#pragma pack( push, 1 )
+	struct ImageHeader final
+	{
+		uint32_t	width;
+		uint32_t	height;
+		uint8_t		bit_depth;			// TODO: Express the enumeration.
+		uint8_t		color_type;			// TODO: Express the enumeration.
+		uint8_t		compression_method;	// TODO: Express the enumeration.
+		uint8_t		filter_method;		// TODO: Express the enumeration.
+		uint8_t		interlace_method;	// TODO: Express the enumeration.
+	};
+	#pragma pack( pop )
 }
 }
 }
