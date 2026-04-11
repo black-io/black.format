8156f60395fe95876b038ae36594b1ad85e7da5f
Frank Stein <dr.frank.stain@gmail.com>
2026-04-11 20:39:23 +0300

    First fields in state of PNG file view.

--- include/png/PngFileView.h
+++ include/png/PngFileView.h
@@ -7,7 +7,17 @@ inline namespace Format
 {
 inline namespace Png
 {
-
+	/**
+	*/
+	class PngFileView final
+	{
+	// Private state.
+	private:
+		std::vector<Internal::Chunk>		m_cunks;
+		const Internal::ImageHeader*		m_header	= nullptr;
+		Black::PlainView<const std::byte>	m_palette;
+		Black::PlainView<const std::byte>	m_image;
+	};
 }
 }
 }
