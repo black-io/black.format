710fbaf23a231f10b2a99cdd396a8c90b77c31c2
Frank Stein <dr.frank.stain@gmail.com>
2026-04-12 22:54:32 +0300

    First code for PNG file view.

--- source/png/PngFileView.cpp
+++ source/png/PngFileView.cpp
@@ -26,16 +26,37 @@ namespace
 	{
 	}
 
-	PngFileView::PngFileView( Black::PlainView<const std::byte> file_memory )
+	PngFileView::PngFileView( PngFileView&& other ) noexcept
+		: m_file_memory{ std::move( other.m_file_memory ) }
+		, m_cunks{ std::move( other.m_cunks ) }
+		, m_header{ std::exchange( other.m_header, nullptr ) }
+		, m_palette{ std::move( other.m_palette ) }
+		, m_image{ std::move( other.m_image ) }
+		, m_is_valid{ std::exchange( other.m_is_valid, false ) }
+		, m_is_parsed{ std::exchange( other.m_is_parsed, false ) }
 	{
 	}
 
-	PngFileView::PngFileView( Black::PlainView<const std::byte> file_memory, const Black::ConstructInplace )
+	PngFileView::PngFileView( Black::PlainView<const std::byte> file_memory ) noexcept
+		: m_file_memory{ std::move( file_memory ) }
 	{
+		TestFileMemory();
+	}
+
+	PngFileView::PngFileView( Black::PlainView<const std::byte> file_memory, const Black::ConstructInplace ) noexcept
+		: PngFileView{ std::move( file_memory ) }
+	{
+		EnsureFileMemoryParsed();
+	}
+
+	PngFileView& PngFileView::operator=( PngFileView&& other ) noexcept
+	{
+		return Black::CopyAndSwap( *this, std::move( other ) );
 	}
 
 	PngFileView& PngFileView::operator=( Black::PlainView<const std::byte> file_memory ) noexcept
 	{
+		return Black::CopyAndSwap( *this, std::move( file_memory ) );
 	}
 
 	void PngFileView::Reset()
@@ -44,6 +65,13 @@ namespace
 
 	void PngFileView::Swap( PngFileView& other )
 	{
+		Black::Swap( m_file_memory, other.m_file_memory );
+		Black::Swap( m_cunks, other.m_cunks );
+		Black::Swap( m_header, other.m_header );
+		Black::Swap( m_palette, other.m_palette );
+		Black::Swap( m_image, other.m_image );
+		Black::Swap( m_is_valid, other.m_is_valid );
+		Black::Swap( m_is_parsed, other.m_is_parsed );
 	}
 
 	const bool PngFileView::HasValidHeader() const
