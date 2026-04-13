44985137b3337e0fa2334fdca48598c02cd4d5a2
Frank Stein <dr.frank.stain@gmail.com>
2026-04-13 13:34:12 +0300

    First code for PNG file structure.

---
+++ include/png/PngStructure.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Png
+{
+	/**
+	*/
+	struct PngStructure final
+	{
+		//
+		using Header = Internal::ImageHeader;
+	};
+}
+}
+}
