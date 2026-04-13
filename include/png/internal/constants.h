7c1df6efad771f17f4bbce6be1a420f3d58fba06
Frank Stein <dr.frank.stain@gmail.com>
2026-04-13 14:59:21 +0300

    Add type code of header chunk.

--- include/png/internal/constants.h
+++ include/png/internal/constants.h
@@ -13,6 +13,9 @@ namespace Internal
 	inline constexpr uint8_t FILE_PREAMBULA[] { 137, 80, 78, 71, 13, 10, 26, 10 };
 
 	//
+	inline constexpr uint32_t TYPE_CODE_IHDR = 'RDHI';
+
+	//
 	inline constexpr uint32_t TYPE_CODE_IEND = 'DNEI';
 }
 }
