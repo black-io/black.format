f7be4dc054a24d59bcf392d11efd8e477c7557d5
Frank Stein <dr.frank.stain@gmail.com>
2026-04-15 11:37:08 +0300

    First look on byte-ordered value.

--- include/.global/ByteOrderedIntegral.h
+++ include/.global/ByteOrderedIntegral.h
@@ -12,6 +12,46 @@ inline namespace Global
 	template< typename TValue, const Black::PlatformEndianness VALUE_ENDIANNESS >
 	class ByteOrderedIntegral final
 	{
+	// Friendship interface.
+	public:
+		friend inline void swap( ByteOrderedIntegral& left, ByteOrderedIntegral& right )	{ left.Swap( right ); };
+
+	// Public life-time management.
+	public:
+		inline ByteOrderedIntegral() noexcept;
+		inline ByteOrderedIntegral( const ByteOrderedIntegral& other ) noexcept;
+		inline ByteOrderedIntegral( ByteOrderedIntegral&& other ) noexcept;
+
+		inline ByteOrderedIntegral( TValue value ) noexcept;
+		inline ByteOrderedIntegral( TValue value, const Black::StoreAsIs ) noexcept;
+
+		inline ~ByteOrderedIntegral() noexcept;
+
+
+		inline ByteOrderedIntegral& operator = ( const ByteOrderedIntegral& other ) noexcept;
+		inline ByteOrderedIntegral& operator = ( ByteOrderedIntegral&& other ) noexcept;
+
+		inline ByteOrderedIntegral& operator = ( TValue value ) noexcept;
+
+	// Public interface.
+	public:
+		//
+		void Swap( PngFileView& other );
+
+
+		//
+		inline void StoreAsIs( TValue value );
+
+
+		//
+		inline TValue GetValue() const;
+
+
+		inline operator TValue () const;
+
+	// Private state.
+	private:
+		TValue	m_value;
 	};
 }
 }
