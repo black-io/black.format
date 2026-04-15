429d0d80cc899f6cdbe73c176440ef18e302a228
Frank Stein <dr.frank.stain@gmail.com>
2026-04-15 12:39:05 +0300

    Add docs to byte-ordered value.

--- include/.global/ByteOrderedIntegral.h
+++ include/.global/ByteOrderedIntegral.h
@@ -8,6 +8,17 @@ inline namespace Format
 inline namespace Global
 {
 	/**
+		@brief	Byte-ordered integral value template.
+
+		This template represents the type of value object, which store the underlying value in potentially different endianness than target platform use.
+		In particular case, the endianness of stored value may be the same as one of target platform.
+		But the major thing here is to fix the endianness out of target platform bounding.
+
+		The role of byte-ordered integral value is crucial in networking of file formats.
+		Underlying type should be trivial, integral and may not be pointer.
+
+		@tparam	TValue				Type of stored value.
+		@tparam	VALUE_ENDIANNESS	Fixed endianness of stored value.
 	*/
 	template< typename TValue, const Black::PlatformEndianness VALUE_ENDIANNESS >
 	class ByteOrderedIntegral final
@@ -35,18 +46,18 @@ inline namespace Global
 
 	// Public interface.
 	public:
-		//
+		// Swap the state of this value with other.
 		inline void Swap( ByteOrderedIntegral& other );
 
 
-		//
+		// Store the given value as is. This method ignores any difference in endianness.
 		inline void SetValueAsIs( TValue value );
 
 
-		//
+		// Get the value converted in endianness of target platform.
 		inline TValue GetValue() const;
 
-		//
+		// Get the value as is. This method ignores any difference in endianness.
 		inline TValue GetValueAsIs() const;
 
 
@@ -54,7 +65,7 @@ inline namespace Global
 
 	// Private state.
 	private:
-		TValue	m_value;
+		TValue	m_value;	// Stored value.
 	};
 }
 }
