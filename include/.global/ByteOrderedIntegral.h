b885ee7390b7e973c95d31721724a48e856d8a36
Frank Stein <dr.frank.stain@gmail.com>
2026-04-15 11:12:23 +0300

    Add declaration of byte-ordered value.

--- include/.global/ByteOrderedIntegral.h
+++ include/.global/ByteOrderedIntegral.h
@@ -7,7 +7,12 @@ inline namespace Format
 {
 inline namespace Global
 {
-
+	/**
+	*/
+	template< typename TValue, const Black::PlatformEndianness VALUE_ENDIANNESS >
+	class ByteOrderedIntegral final
+	{
+	};
 }
 }
 }
