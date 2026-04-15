56c651b3daf842af6b63c9aaa991bf9b18cea456
Frank Stein <dr.frank.stain@gmail.com>
2026-04-15 13:23:08 +0300

    Make the fields of PNG image header of BE order.

--- include/png/internal/structures.h
+++ include/png/internal/structures.h
@@ -33,13 +33,13 @@ namespace Internal
 	#pragma pack( push, 1 )
 	struct ImageHeader final
 	{
-		uint32_t	width;
-		uint32_t	height;
-		uint8_t		bit_depth;			// TODO: Express the enumeration.
-		uint8_t		color_type;			// TODO: Express the enumeration.
-		uint8_t		compression_method;	// TODO: Express the enumeration.
-		uint8_t		filter_method;		// TODO: Express the enumeration.
-		uint8_t		interlace_method;	// TODO: Express the enumeration.
+		Black::BigEndianIntegral<uint32_t>	width;
+		Black::BigEndianIntegral<uint32_t>	height;
+		uint8_t								bit_depth;			// TODO: Express the enumeration.
+		uint8_t								color_type;			// TODO: Express the enumeration.
+		uint8_t								compression_method;	// TODO: Express the enumeration.
+		uint8_t								filter_method;		// TODO: Express the enumeration.
+		uint8_t								interlace_method;	// TODO: Express the enumeration.
 	};
 	#pragma pack( pop )
 
