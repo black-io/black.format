7a71bf3e9fbc63fb24940b6c1ddf9ae0018fb6b8
Frank Stein <dr.frank.stain@gmail.com>
2026-04-17 22:53:34 +0300

    Add code of chunk type formatting.

--- source/png/internal/constants.operations.cpp
+++ source/png/internal/constants.operations.cpp
@@ -16,6 +16,52 @@ namespace
 }
 
 
+	std::string_view format_as( const ChunkTypeCode value )
+	{
+		switch( value )
+		{
+		case ChunkTypeCode::ImageHeader:
+			return "IHDR";
+		case ChunkTypeCode::Palette:
+			return "PLTE";
+		case ChunkTypeCode::ImageData:
+			return "IDAT";
+		case ChunkTypeCode::ImageEnd:
+			return "IEND";
+		case ChunkTypeCode::Transparency:
+			return "tRNS";
+		case ChunkTypeCode::ImageGamma:
+			return "gAMA";
+		case ChunkTypeCode::Chromaticities:
+			return "cHRM";
+		case ChunkTypeCode::SrgbColorSpace:
+			return "sRGB";
+		case ChunkTypeCode::EmbeddedIcc:
+			return "iCCP";
+		case ChunkTypeCode::TextualData:
+			return "tEXt";
+		case ChunkTypeCode::CompressedTextualData:
+			return "zTXt";
+		case ChunkTypeCode::InternationalTextualData:
+			return "iTXt";
+		case ChunkTypeCode::BackgroundColor:
+			return "bKGD";
+		case ChunkTypeCode::PhysicalDimensions:
+			return "pHYs";
+		case ChunkTypeCode::SignificantBits:
+			return "sBIT";
+		case ChunkTypeCode::SuggestedPalette:
+			return "sPLT";
+		case ChunkTypeCode::PaletteHistogram:
+			return "hIST";
+		case ChunkTypeCode::LastModificationTime:
+			return "tIME";
+		default:
+			break;
+		};
+
+		return "Unknown";
+	}
 }
 }
 }
