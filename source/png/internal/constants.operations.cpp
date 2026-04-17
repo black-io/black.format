b72fe19c2784b6862af1e3390663bbd5729ceb01
Frank Stein <dr.frank.stain@gmail.com>
2026-04-17 18:58:13 +0300

    Regular code for implementations of operations.

---
+++ source/png/internal/constants.operations.cpp
@@ -0,0 +1,22 @@
+#include <black/format/png.h>
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Png
+{
+namespace Internal
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/PNG/Operations";
+}
+
+
+}
+}
+}
+}
