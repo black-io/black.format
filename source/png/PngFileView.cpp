e28e18b8157f2771c4e2b6923383fee5c436bf40
Frank Stein <dr.frank.stain@gmail.com>
2026-04-19 21:35:26 +0300

    Fix the code of header check.

--- source/png/PngFileView.cpp
+++ source/png/PngFileView.cpp
@@ -168,7 +168,7 @@ namespace
 	const bool PngFileView::HasValidHeader() const
 	{
 		const Black::PlainView<const PngFileView::ChunkEntry> chunks{ GetChunks() };
-		return !chunks.empty() && chunks.front().header->type_code == Internal::TYPE_CODE_IHDR;
+		return !chunks.empty() && chunks.front().header->type_code == Internal::ChunkTypeCode::ImageHeader;
 	}
 
 	const bool PngFileView::HasValidFooter() const
