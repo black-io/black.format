d18f5a0cf86298e53b19190ac2c33362306075b3
Frank Stein <dr.frank.stain@gmail.com>
2026-04-23 10:23:16 +0300

    Add docs for BE integral value.

--- include/.global/aliases.h
+++ include/.global/aliases.h
@@ -25,6 +25,18 @@ inline namespace Global
 	using LittleEndianIntegral = ByteOrderedIntegral<TValue, Black::PlatformEndianness::LittleEndian>;
 
 	/**
+		@brief	Platform-independent storage for Big-Endian integral value or enumeration.
+
+		This value behaves like regular. It consumes values in platform-specific endianness. It can be implicitly converted to underlying value
+		of platform-specific endianness. But internally it stored always in Big-Endian byte order.
+
+		Such behavior is crucial for file- or network formats, where the values should be stored in particular endianness regardless
+		to platform-specific endianness.
+
+		This type safely consumes enumerations as well. It converts the order of bytes in enumerations, so for user of this type there will be no problem
+		to use enumeration.
+
+		@tparam	TValue	Type of underlying value. May be integral or enumeration.
 	*/
 	template< typename TValue >
 	using BigEndianIntegral = ByteOrderedIntegral<TValue, Black::PlatformEndianness::BigEndian>;
