ea13f844e285bd85e907ef9d7e4895a353ba0b28
Frank Stein <dr.frank.stain@gmail.com>
2026-04-23 10:09:30 +0300

    Add docs for LE integral value.

--- include/.global/aliases.h
+++ include/.global/aliases.h
@@ -8,6 +8,15 @@ inline namespace Format
 inline namespace Global
 {
 	/**
+		@brief	Platform-independent storage for Little-Endian integral value or enumeration.
+
+		This value behaves like regular. It consumes values in platform-specific endianness. It can be implicitly converted to underlying value
+		of platform-specific endianness. But internally it stored always in Little-Endian byte order.
+
+		Such behavior is crucial for file- or network formats, where the values should be stored in particular endianness regardless
+		to platform-specific endianness.
+
+		@tparam	TValue	Type of underlying value.
 	*/
 	template< typename TValue >
 	using LittleEndianIntegral = ByteOrderedIntegral<TValue, Black::PlatformEndianness::LittleEndian>;
