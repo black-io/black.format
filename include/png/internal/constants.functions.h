c924c9f512bfe4e4aec5707f803ee3d90be24a01
Frank Stein <dr.frank.stain@gmail.com>
2026-04-24 09:08:26 +0300

    Add docs for color type verification function.

--- include/png/internal/constants.functions.h
+++ include/png/internal/constants.functions.h
@@ -21,7 +21,20 @@ namespace Internal
 	*/
 	const bool IsBitDepthValid( const BitDepth bit_depth );
 
-	//
+	/**
+		@brief	Whether the given color type is valid.
+
+		This check complies to section 4.1.1 (IHDR Image header) of PNG 1.2 file format specification.
+		Color type is a single-byte integer that describes the interpretation of the image data.
+		Color type codes represent sums of the following values:
+		- 1 (palette used),
+		- 2 (color used),
+		- 4 (alpha channel used).
+		Valid values are 0, 2, 3, 4, and 6.
+
+		@param	color_type	Given color type to be checked.
+		@return				`true` for valid values. `false` in other cases.
+	*/
 	const bool IsColorTypeValid( const ColorType color_type );
 
 	//
