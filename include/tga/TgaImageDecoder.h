f5428fa228bb846f4e7de2a476f55ceaa8547d53
Frank Stein <dr.frank.stain@gmail.com>
2026-05-08 15:11:44 +0300

    Fix noexcept modifier.

--- include/tga/TgaImageDecoder.h
+++ include/tga/TgaImageDecoder.h
@@ -40,7 +40,7 @@ inline namespace Tga
 		inline ~TgaImageDecoder() noexcept = default;
 
 
-		inline TgaImageDecoder& operator = ( const TgaImageDecoder& other ) noexcept	= default;
+		inline TgaImageDecoder& operator = ( const TgaImageDecoder& other )				= default;
 		inline TgaImageDecoder& operator = ( TgaImageDecoder&& other ) noexcept			{ return Black::CopyAndSwap( *this, std::move( other ) ); };
 
 		inline TgaImageDecoder& operator = ( const TgaStructure::Header& input_header )	{ return Black::CopyAndSwap( *this, input_header ); };
