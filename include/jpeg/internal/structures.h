c33a61c59ccca2b9cb0fcf4e3ad38f0008a865ab
Frank Stein <dr.frank.stain@gmail.com>
2026-07-02 22:08:12 +0300

    Apply th 1b-packing for JIF marker type.

--- include/jpeg/internal/structures.h
+++ include/jpeg/internal/structures.h
@@ -9,11 +9,13 @@ inline namespace Jpeg
 {
 namespace Internal
 {
+	#pragma pack( push, 1 )
 	struct Marker final
 	{
 		std::byte	prefix;
 		MarkerCode	code;
 	};
+	#pragma pack( pop )
 
 	struct SegmentHeader final
 	{
