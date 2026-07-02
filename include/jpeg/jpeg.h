de558caf7df4e636b2daed5548e311bbc088cde1
Frank Stein <dr.frank.stain@gmail.com>
2026-07-02 14:48:58 +0300

    First code for master-header of JPEG format subsystem.

---
+++ include/jpeg/jpeg.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+// Fundamental external dependencies.
+
+
+namespace Black
+{
+inline namespace Format
+{
+// JPEG file format subsystem.
+inline namespace Jpeg
+{
+// Internal definitions for JPEG format.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Internal stuff.
+
+// PNG file format types.
+
+// Deferred inline implementations.
