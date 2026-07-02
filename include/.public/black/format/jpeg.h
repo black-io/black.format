2535db859430d39e18ee8f7d5d8475da293c83bb
Frank Stein <dr.frank.stain@gmail.com>
2026-07-02 14:45:40 +0300

    Fill the publick header of JPEG format subsystem.

---
+++ include/.public/black/format/jpeg.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// JPEG file format.
+#include "../../../jpeg/jpeg.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
