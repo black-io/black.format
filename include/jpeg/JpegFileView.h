4deb9fb4b6ee3de3416b27568a33ebe838b40d46
Frank Stein <dr.frank.stain@gmail.com>
2026-07-06 11:37:31 +0300

    Define most basic life-time methods.

--- include/jpeg/JpegFileView.h
+++ include/jpeg/JpegFileView.h
@@ -9,6 +9,22 @@ inline namespace Jpeg
 {
 	class JpegFileView final
 	{
+	// Public life-time management.
+	public:
+		inline JpegFileView() noexcept						= default;
+		inline JpegFileView( const JpegFileView& other )	= default;
+		JpegFileView( JpegFileView&& other ) noexcept;
+
+		explicit JpegFileView( Black::PlainView<const std::byte> file_memory ) noexcept;
+		JpegFileView( Black::PlainView<const std::byte> file_memory, const Black::ConstructInplace ) noexcept;
+
+		inline ~JpegFileView() noexcept = default;
+
+
+		inline JpegFileView& operator = ( const JpegFileView& other )		= default;
+		inline JpegFileView& operator = ( JpegFileView&& other ) noexcept	{ return Black::CopyAndSwap( *this, std::move( other ) ); };
+
+		JpegFileView& operator = ( Black::PlainView<const std::byte> file_memory ) noexcept;
 	};
 }
 }
