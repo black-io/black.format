5f9b3de0173d15dadf78ddb2c0339327d13a8561
Frank Stein <dr.frank.stain@gmail.com>
2026-07-13 15:14:01 +0300

    Define the structure of JFIF header.

--- include/jpeg/internal/structures.h
+++ include/jpeg/internal/structures.h
@@ -25,6 +25,19 @@ namespace Internal
 	};
 	#pragma pack( pop )
 
+	#pragma pack( push, 1 )
+	struct JfifHeader final
+	{
+		char		signature[5];
+		uint8_t		version[2];
+		uint8_t		units;
+		uint16_t	width;
+		uint16_t	height;
+		uint8_t		thumbnail_width;
+		uint8_t		thumbnail_height;
+	};
+	#pragma pack( pop )
+
 	struct SegmentEntry final
 	{
 		const SegmentHeader*				header;
