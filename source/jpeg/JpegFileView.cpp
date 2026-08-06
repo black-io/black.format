5811293434abd69b510b572000abb9153391bd37
Frank Stein <dr.frank.stain@gmail.com>
2026-08-06 22:52:17 +0300

    Enable swapping of image blocks.

--- source/jpeg/JpegFileView.cpp
+++ source/jpeg/JpegFileView.cpp
@@ -206,6 +206,7 @@ namespace
 		Black::Swap( m_file_memory, other.m_file_memory );
 		Black::Swap( m_markers, other.m_markers );
 		Black::Swap( m_segments, other.m_segments );
+		Black::Swap( m_image_blocks, other.m_image_blocks );
 		Black::Swap( m_is_valid, other.m_is_valid );
 		Black::Swap( m_is_parsed, other.m_is_parsed );
 	}
