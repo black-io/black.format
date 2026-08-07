e66979217e7c7760dc1c7e1a19c35580357b74a2
Frank Stein <dr.frank.stain@gmail.com>
2026-08-07 15:10:11 +0300

    Add field to point the frame header from SOF segment.

--- include/jpeg/JpegFileView.h
+++ include/jpeg/JpegFileView.h
@@ -88,6 +88,7 @@ inline namespace Jpeg
 		mutable std::vector<Internal::ImageBlockEntry>	m_image_blocks;
 
 		mutable const Internal::JfifHeader*		m_jfif_header	= nullptr;
+		mutable const Internal::FrameHeader*	m_frame_header	= nullptr;
 
 		mutable bool	m_is_valid	= false;
 		mutable bool	m_is_parsed	= false;
