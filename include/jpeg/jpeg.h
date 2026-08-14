2f7198bf0c97b3539d26190fc98f5764c3b464fa
Frank Stein <dr.frank.stain@gmail.com>
2026-08-14 20:46:51 +0300

    Add instruction to include the helper functions for internal constants.

--- include/jpeg/jpeg.h
+++ include/jpeg/jpeg.h
@@ -24,6 +24,7 @@ namespace Internal
 // Internal stuff.
 #include "internal/constants.h"
 #include "internal/structures.h"
+#include "internal/constants.functions.h"
 
 // JPEG file format types.
 #include "JpegStructure.h"
