ca43fabcdf7eda0900f8115fecb78916dcf62cb6
Frank Stein <dr.frank.stain@gmail.com>
2026-08-14 21:22:23 +0300

    Add docs for marker code validation predicate.

--- include/jpeg/internal/constants.functions.h
+++ include/jpeg/internal/constants.functions.h
@@ -10,6 +10,16 @@ inline namespace Jpeg
 namespace Internal
 {
 	/**
+		@brief	Whether the given candidate may be considered as valid marker code.
+
+		Valid code of JIF marker is:
+		- not `0x00`,
+		- not `0xFF`,
+		- not private (aka 0x01),
+		- not reserved (aka 0x02..0x0BF).
+
+		@param	candidate	Given code to be checked.
+		@return				`true` for valid code of JIF marker and `false` in other way.
 	*/
 	const bool IsMarkerCodeValid( const MarkerCode candidate );
 }
