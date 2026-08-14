1b71034a5439828458d7a43b6262cd45e819c3d4
Frank Stein <dr.frank.stain@gmail.com>
2026-08-14 21:02:11 +0300

    Fix the qualifiers of local constants.

--- source/jpeg/internal/constants.functions.cpp
+++ source/jpeg/internal/constants.functions.cpp
@@ -14,9 +14,9 @@ namespace
 	// Logging channel.
 	constexpr const char* LOG_CHANNEL = "Black/JPEG/Functions";
 
-	inline constexpr MarkerCode INVALID_CODE_1	= MarkerCode( 0x00U );
-	inline constexpr MarkerCode INVALID_CODE_2	= MarkerCode( 0xFFU );
-	inline constexpr MarkerCode MIN_CODE		= MarkerCode::Sof0;
+	const MarkerCode INVALID_CODE_1	= MarkerCode( 0x00U );
+	const MarkerCode INVALID_CODE_2	= MarkerCode( 0xFFU );
+	const MarkerCode MIN_CODE		= MarkerCode::Sof0;
 }
 
 
