052bfeaee67376a2a420cceb4a7afa6bce727339
Frank Stein <dr.frank.stain@gmail.com>
2026-08-15 13:41:11 +0300

    Add most basic code for operations source.

---
+++ source/jpeg/internal/constants.operations.cpp
@@ -0,0 +1,22 @@
+#include <black/format/jpeg.h>
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Jpeg
+{
+namespace Internal
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/JPEG/Operations";
+}
+
+
+}
+}
+}
+}
