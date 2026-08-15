8388c51a47b41de840589e7724923e6bf93baf38
Frank Stein <dr.frank.stain@gmail.com>
2026-08-15 11:15:08 +0300

    Declare JFIF density units validation predicate.

--- include/jpeg/internal/constants.functions.h
+++ include/jpeg/internal/constants.functions.h
@@ -22,6 +22,10 @@ namespace Internal
 		@return				`true` for valid code of JIF marker and `false` in other way.
 	*/
 	const bool IsMarkerCodeValid( const MarkerCode candidate );
+
+	/**
+	*/
+	const bool IsDesityUnitValid( const DensityUnit candidate );
 }
 }
 }
