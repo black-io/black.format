a15964cf7602aa8ab63f451a36134dc8fb9562d8
Frank Stein <dr.frank.stain@gmail.com>
2026-08-16 12:48:59 +0300

    Add code of JIF marker formatter.

--- source/jpeg/internal/constants.operations.cpp
+++ source/jpeg/internal/constants.operations.cpp
@@ -18,6 +18,27 @@ namespace
 
 	std::string_view format_as( const MarkerCode value )
 	{
+		static constexpr std::string_view code_names[] {
+			"SOF0",		"SOF1",		"SOF2",	"SOF3",
+			"DHT",
+			"SOF5",		"SOF6",		"SOF7",
+			"JPG",
+			"SOF9",		"SOF10",	"SOF11",
+			"DAC",
+			"SOF13",	"SOF14",	"SOF15",
+			"RST0",		"RST1",		"RST2",		"RST3",		"RST4",		"RST5",		"RST6",		"RST7",
+			"SOI",		"EOI",		"SOS",		"DQT",		"DNL",		"DRI",		"DHP",		"EXP",
+			"APP0",		"APP1",		"APP2",		"APP3",		"APP4",		"APP5",		"APP6",		"APP7",		"APP8",		"APP9",		"APP10",
+			"APP11",	"APP12",	"APP13",	"APP14",	"APP15",
+			"JPG0",		"JPG1",		"JPG2",		"JPG3",		"JPG4",		"JPG5",		"JPG6",		"JPG7",		"JPG8",		"JPG9",		"JPG10",
+			"JPG11",	"JPG12",	"JPG13",
+			"COM",
+		};
+
+		CRET( !IsMarkerCodeValid( value ), "Unknown" );
+
+		const size_t code_index = Black::GetEnumValue( value ) - Black::GetEnumValue( MarkerCode::Sof0 );
+		return code_names[ code_index ];
 	}
 }
 }
