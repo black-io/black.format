960c0cf12a58fd217940cea758e8d2c7c3ed579f
Frank Stein <dr.frank.stain@gmail.com>
2026-08-16 21:45:21 +0300

    Add note for density height member.

--- include/jpeg/internal/structures.h
+++ include/jpeg/internal/structures.h
@@ -67,7 +67,7 @@ namespace Internal
 		struct
 		{
 			Black::BigEndianIntegral<uint16_t>	x;
-			Black::BigEndianIntegral<uint16_t>	y;
+			Black::BigEndianIntegral<uint16_t>	y;									// Density of image height.
 		}										density;							// Image density. The meaning of values depends on `units` value.
 
 		struct
