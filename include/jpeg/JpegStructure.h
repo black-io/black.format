ef4e1046663562826e69c6945b17afd7f8058aff
Frank Stein <dr.frank.stain@gmail.com>
2026-08-17 12:43:55 +0300

    Add doc for JPEG file structure.

--- include/jpeg/JpegStructure.h
+++ include/jpeg/JpegStructure.h
@@ -7,6 +7,15 @@ inline namespace Format
 {
 inline namespace Jpeg
 {
+	/**
+		@brief	Representation of JPEG file internal structure.
+
+		Here are collected internal types of JPEG file format. All of it aggregated in certain structures by its meaning.
+		In any case any of internal JPEG-file types needed by consumer's code, it may be accessed via `JpegStructure`.
+
+		JPEG file format is complex a lot, since it is not the single file format. Different file format are hidden under the hood of single 'jpg' extension.
+		So more than some single document is required to understand internals of JPEG file.
+	*/
 	struct JpegStructure final
 	{
 		//
