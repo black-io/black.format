c1346eb6abb4792f746fc262a1fd252335936720
Frank Stein <dr.frank.stain@gmail.com>
2026-08-17 11:01:09 +0300

    Add note for image width member.

--- include/jpeg/internal/structures.h
+++ include/jpeg/internal/structures.h
@@ -146,7 +146,7 @@ namespace Internal
 	{
 		uint8_t								image_precision;	// (P) Sample precission, in bits.
 		Black::BigEndianIntegral<uint16_t>	image_height;		// (Y) Number of lines in the source image.
-		Black::BigEndianIntegral<uint16_t>	image_width;
+		Black::BigEndianIntegral<uint16_t>	image_width;		// (X) Number of samples per line in the source image.
 		uint8_t								components_count;
 	};
 	#pragma pack( pop )
