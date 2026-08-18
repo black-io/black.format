d97c418653501c2de6e21703c3e9fc74b74c236b
Frank Stein <dr.frank.stain@gmail.com>
2026-08-18 22:24:50 +0300

    Add note for segment list field.

--- include/jpeg/JpegFileView.h
+++ include/jpeg/JpegFileView.h
@@ -125,7 +125,7 @@ inline namespace Jpeg
 	// Private non-state.
 	private:
 		mutable std::vector<const Internal::Marker*>	m_markers;		// List of markers, found in file memory.
-		mutable std::vector<Internal::SegmentEntry>		m_segments;
+		mutable std::vector<Internal::SegmentEntry>		m_segments;		// List of segments, found in file memory.
 		mutable std::vector<Internal::ImageBlockEntry>	m_image_blocks;
 
 		mutable const Internal::JfifHeader*		m_jfif_header	= nullptr;
