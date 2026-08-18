ac8ffc9ed56ffe38cf75159572cb9e37c77dcbf0
Frank Stein <dr.frank.stain@gmail.com>
2026-08-18 09:49:09 +0300

    Add first documentation for JPEG file view.

--- include/jpeg/JpegFileView.h
+++ include/jpeg/JpegFileView.h
@@ -7,6 +7,15 @@ inline namespace Format
 {
 inline namespace Jpeg
 {
+	/**
+		@brief	View of regualr JPEG file.
+
+		This type can be used to verify that some particular file satisfy the basic requirements of valid JPEG file or even has
+		valid internal structure of JPEG file.
+		The view doesn't own given memory. It doesn't copy the memory of file nor create some expensive objects.
+		It can't decode stored image or change the content of segments.
+		This view just gives to user the easiest way to access the data of stored image.
+	*/
 	class JpegFileView final
 	{
 	// Friendship interface.
