09729ff333fa61aee0d45bb38f84dd190d557bfe
Frank Stein <dr.frank.stain@gmail.com>
2026-08-18 09:50:30 +0300

    Add doc for reset method.

--- include/jpeg/JpegFileView.h
+++ include/jpeg/JpegFileView.h
@@ -66,7 +66,7 @@ inline namespace Jpeg
 
 	// Public interface.
 	public:
-		//
+		// Perform the full reset of file view. As result, the view become like just default-constructed.
 		void Reset();
 
 		//
