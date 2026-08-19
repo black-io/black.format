83c01587b0815ec9b105dabc27175bf488ee1d29
Frank Stein <dr.frank.stain@gmail.com>
2026-08-19 13:06:12 +0300

    Add doc for padding detection predicate.

--- source/jpeg/JpegFileView.cpp
+++ source/jpeg/JpegFileView.cpp
@@ -18,6 +18,7 @@ namespace
 
 namespace
 {
+	// Whether the next bytes of buffer represent padding sequence. Such padding may be skipped.
 	const bool IsPaddingSequence( const Black::PlainView<const std::byte>& buffer )
 	{
 		EXPECTS_DEBUG( buffer.GetLength() >= sizeof( Internal::Marker ) );
