e7fa4547cf9064a71c0ac3eca87b283053bada19
Frank Stein <dr.frank.stain@gmail.com>
2026-08-20 11:43:14 +0300

    Add files for JIF marker statistics.

---
+++ source/jpeg/MarkerStats.cpp
@@ -0,0 +1,21 @@
+#include <black/format/jpeg.h>
+
+#include "MarkerStats.h"
+
+
+namespace Black
+{
+inline namespace Format
+{
+inline namespace Jpeg
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/JPEG/MarkerStats";
+}
+
+
+}
+}
+}
