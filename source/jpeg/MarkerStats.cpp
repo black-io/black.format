eec5b299b914902cf3fc0bac0e118318662b49a1
Frank Stein <dr.frank.stain@gmail.com>
2026-08-20 12:43:35 +0300

    Fill the code of conversion constrcuctor.

--- source/jpeg/MarkerStats.cpp
+++ source/jpeg/MarkerStats.cpp
@@ -17,7 +17,9 @@ namespace
 
 
 	MarkerStats::MarkerStats( Black::PlainView<const std::byte> file_memory ) noexcept
+		: m_file_memory{ std::move( file_memory ) }
 	{
+		std::fill( std::begin( m_marker_positions ), std::end( m_marker_positions ), Black::UNDEFINED_INDEX );
 	}
 }
 }
